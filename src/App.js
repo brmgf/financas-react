992d1505c8632b20b9515eb4e7a34f3d16f6a9db
milene <milenefaria33@gmail.com>
2023-04-22 11:59:58 -0300

    criacao do projeto e tela de login

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./view/login";
+
+import 'bootswatch/dist/flatly/bootstrap.css';
+import './custom.css'
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default App;
