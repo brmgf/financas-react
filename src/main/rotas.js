40d8b8b0ab1552dd4cfa02c02dab3b27ac348e1f
milene <milenefaria33@gmail.com>
2023-05-03 22:46:48 -0300

    criacao da tela inicial

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import {Route, Switch, HashRouter} from 'react-router-dom'
 import Login from "../view/login";
+import Home from "../view/home";
 import CadastroUsuario from "../view/cadastroUsuario";
 
 function Rotas() {
     return (
         <HashRouter>
             <Switch>
+                <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/novo-usuario" component={CadastroUsuario} />
             </Switch>
